1daf4d276b0e87b683089aa303d15e71f6687d0f
Fabricio Carvalho <fcarvalho1999@gmail.com>
2021-08-07 20:38:43 -0300

    choice of password length update

--- script.js
+++ script.js
@@ -1,52 +1,51 @@
-function generate(){
-    var chars = "";
-    var passlength;
-    var password = "";
-    var min = document.getElementById('min');
-    var max = document.getElementById("max");
-    var number = document.getElementById("number");
-    var esp = document.getElementById("esp");
-    var eight = document.getElementById("eight");
-    var sixteen = document.getElementById("sixteen");
+function generate() {
+  var chars = "";
+  var passlength;
+  var password = "";
+  var min = document.getElementById("min");
+  var max = document.getElementById("max");
+  var number = document.getElementById("number");
+  var esp = document.getElementById("esp");
+  var eight = document.getElementById("eight");
+  var sixteen = document.getElementById("sixteen");
 
-    
-    if (min.checked == true){
-        chars = chars + "abcdefghijklmnopqrstuvwxyz";
-    }
-    if (max.checked == true){
-        chars = chars + "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-    }
-    if (number.checked == true){
-        chars = chars + "1234567890";
-    }
-    if (esp.checked == true){
-        chars = chars + "@%+/'!#$^?:.)(}{][~-_'";
-    }
+  if (min.checked == true) {
+    chars = chars + "abcdefghijklmnopqrstuvwxyz";
+  }
+  if (max.checked == true) {
+    chars = chars + "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  }
+  if (number.checked == true) {
+    chars = chars + "1234567890";
+  }
+  if (esp.checked == true) {
+    chars = chars + "@%+/'!#$^?:.)(}{][~-_'";
+  }
 
-    console.log(chars);
-    
-    if (eight.checked == true){
-        passlength = 8;
-    }
-    if (sixteen.checked == true){
-        passlength = 16;
-    }
-    for (var i = 0; i < passlength; i++){
-        var randomNum = Math.floor(Math.random() * chars.length);
-        password += chars.substring(randomNum, randomNum + 1);
-    }
-    document.getElementById("password").value = password;
+  console.log(chars);
+
+  passlength = document.getElementById("password-length").value;
+
+  for (var i = 0; i < passlength; i++) {
+    var randomNum = Math.floor(Math.random() * chars.length);
+    password += chars.substring(randomNum, randomNum + 1);
+  }
+  document.getElementById("password").value = password;
 }
 
 function copyText() {
-    let copyTxt = document.getElementById('password');
+  let copyTxt = document.getElementById("password");
 
-    copyTxt.select();
-    copyTxt.setSelectionRange(0,99999);
+  copyTxt.select();
+  copyTxt.setSelectionRange(0, 99999);
 
-    document.execCommand('copy');
+  document.execCommand("copy");
 
-    if (copyTxt.value != '') {
-    alert('Text Copied: ' + copyTxt.value);   
-    }
+  if (copyTxt.value != "") {
+    alert("Text Copied: " + copyTxt.value);
+  }
+}
+
+function updateTextInput(value) {
+  document.getElementById("password-length").value = value;
 }
