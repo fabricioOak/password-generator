cbe38bdb13d0e2b69b5d9e0629495b6066b64ac2
Fabricio Carvalho <fcarvalho1999@gmail.com>
2021-10-23 10:36:54 -0300

    warning alerts added:   index.html 	warning alerts added:   script.js

--- script.js
+++ script.js
@@ -2,12 +2,11 @@ function generate() {
   var chars = "";
   var passlength;
   var password = "";
+  var alert = document.getElementById("alert");
   var min = document.getElementById("min");
   var max = document.getElementById("max");
   var number = document.getElementById("number");
   var esp = document.getElementById("esp");
-  var eight = document.getElementById("eight");
-  var sixteen = document.getElementById("sixteen");
 
   if (min.checked == true) {
     chars = chars + "abcdefghijklmnopqrstuvwxyz";
@@ -31,6 +30,36 @@ function generate() {
     password += chars.substring(randomNum, randomNum + 1);
   }
   document.getElementById("password").value = password;
+
+  //check if any type of character is selected
+  if (min.checked == false && max.checked == false && number.checked == false && esp.checked == false) {
+    alert.innerHTML = "Please select at least one character type";
+  } else {
+    alert.innerHTML = "";
+  }
+
+  if (password.length > 16) {
+    alert.innerHTML = "Are you out of your mind?";
+    alert.style.color = "red";
+  }
+
+  else if (password.length <= 16 && password.length >= 8) {
+    alert.innerHTML = "That's a good password";
+    alert.style.color = "blue";
+  }
+
+  else if (password.length < 8) {
+    alert.innerHTML = "Afraid of forgetting longer passwords?";
+    alert.style.color = "orange";
+  }
+  else {
+    alert.innerHTML = "Choose character types";
+  }
+
+  if (number.checked == true && esp.checked == true && password.length >= 18) {
+    alert.innerHTML = "If you remember this, you are a machine *_*";
+    alert.style.color = "purple";
+  }
 }
 
 function copyText() {
@@ -49,3 +78,4 @@ function copyText() {
 function updateTextInput(value) {
   document.getElementById("password-length").value = value;
 }
+
